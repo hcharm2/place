f879ee4e60875122488c36764fbd84420ca9b965
chnhanna <chn6450@naver.com>
2019-07-02 02:45:12 +0900

    swagger 추가

--- place/src/main/java/com/chn/place/boot/controller/PageController.java
+++ place/src/main/java/com/chn/place/boot/controller/PageController.java
@@ -3,9 +3,13 @@ package com.chn.place.boot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.annotations.ApiOperation;
+
 @Controller
 public class PageController {
+	
 	@RequestMapping("/")
+	@ApiOperation(value = "사용자 리스트", notes = "사용자 리스트를 가져옵니다.")
 	public String index() {
 		return "index";
 	}
